fca2b8013d9cf514dab673e211774b9503b7e471
Bakir Karovic <bakir.karovic@gmail.com>
2021-06-01 00:57:29 +0200

    added 404 page

---
+++ BestHealtStrategies/BestHealtStrategies/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestHealtStrategies.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error")]
+        public IActionResult UrlNotFound()
+        {
+            ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found. Status code: 404";
+            //var statusCodeResult = HttpContext.Features.Get<IStatusCodeReExecuteFeature>();
+            //ViewBag.Path = statusCodeResult.OriginalPath;
+            //ViewBag.QueryString = statusCodeResult.OriginalQueryString;
+            return View("NotFound");
+        }
+    }
+}
