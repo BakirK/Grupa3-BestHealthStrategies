f39d4206121f90b5624a61bc79c0b0928162bb97
Bakir Karovic <bakir.karovic@gmail.com>
2021-06-01 01:33:29 +0200

    Revert "added 404 page"
    This reverts commit fca2b8013d9cf514dab673e211774b9503b7e471.

--- BestHealtStrategies/BestHealtStrategies/Startup.cs
+++ BestHealtStrategies/BestHealtStrategies/Startup.cs
@@ -59,7 +59,6 @@ namespace BestHealtStrategies
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-                app.UseStatusCodePagesWithReExecute("/Error/{0}");
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
